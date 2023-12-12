3250ad72d6921e565dd451b3df5e9ee5a2657777
Sayed Jaffar <jaff0040@algonquinlive.com>
2023-12-12 14:41:04 -0500

    first commit

---
+++ ReadMe/ReadMe.md
@@ -0,0 +1,5 @@
+# Sayed_Jaffar_FinalProject
+
+When 'x=3' , that means 'x + 2 = 5'
+
+![img](https://wwww.google.com./images/Sayed.png)
---
+++ getDayChristmas/main.js
@@ -0,0 +1,35 @@
+
+
+function getDayChristmas() {
+    DayChristmas (function () {
+      console.log("DayChristmas");
+      callback();
+    },2000)
+  }
+  function DayChristmas() {
+      console.log("DayChristmas");
+  }
+
+
+  const todayDate = new Date();  
+console.log(todayDate);
+
+let day = todayDate.getDay();
+console.log(day);
+
+let month = todayDate.getMonth()
+console.log(month);
+
+let hour = todayDate.getHours();
+console.log(hour);
+
+let DayChristmas = new Date(2023, 12, 25);
+console.log(myBirthday);
+
+let year = todayDate.getYear();
+console.log(year);
+
+
+
+
+    console.log("DayChristmas")
